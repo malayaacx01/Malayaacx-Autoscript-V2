6503ee84f7b6cad5eb330175e8330a5dbdabe77b
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-08 20:48:58 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -290,7 +290,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
-echo -e " $COLOR1│${NC} ${COLBG1}               ${WH} •  STORES  PREMIUM •             ${NC} $COLOR1│ $NC"
+echo -e " $COLOR1│${NC} ${COLBG1}               ${WH} •  MALAYAACX01 - SHOP PREMIUM •             ${NC} $COLOR1│ $NC"
 echo -e " $COLOR1╰══════════════════════════════════════════════════════════╯${NC}"
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ OS            ${COLOR1}: ${WH}$MODEL2${NC}"
