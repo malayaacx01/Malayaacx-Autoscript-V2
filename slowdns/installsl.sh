1a7d47296600b70433b14a7f6178af955b637663
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-08 09:15:12 +0800

    Update installsl.sh

--- slowdns/installsl.sh
+++ slowdns/installsl.sh
@@ -47,15 +47,15 @@ ns_domain_cloudflare() {
 setup_dnstt() {
 	cd
 	mkdir -p /etc/slowdns
-	wget -O dnstt-server "https://raw.githubusercontent.com/litfina/autoscript-vip/main/slowdns/dnstt-server" >/dev/null 2>&1
+	wget -O dnstt-server "https://scvpsv2.malayaacx.my.id/slowdns/dnstt-server" >/dev/null 2>&1
 	chmod +x dnstt-server >/dev/null 2>&1
-	wget -O dnstt-client "https://raw.githubusercontent.com/litfina/autoscript-vip/main/slowdns/dnstt-client" >/dev/null 2>&1
+	wget -O dnstt-client "https://scvpsv2.malayaacx.my.id/slowdns/dnstt-client" >/dev/null 2>&1
 	chmod +x dnstt-client >/dev/null 2>&1
 	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
 	chmod +x *
 	mv * /etc/slowdns
-	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/litfina/autoscript-vip/main/slowdns/client" >/dev/null 2>&1
-	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/litfina/autoscript-vip/main/slowdns/server" >/dev/null 2>&1
+	wget -O /etc/systemd/system/client.service "https://scvpsv2.malayaacx.my.id/slowdns/client" >/dev/null 2>&1
+	wget -O /etc/systemd/system/server.service "https://scvpsv2.malayaacx.my.id/slowdns/server" >/dev/null 2>&1
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
 }
