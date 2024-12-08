0887bb59c4188b3ffbd857df1080ffe0fa5c4de0
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-08 20:10:03 +0800

    Update bckpbot.sh

--- menu/bckpbot.sh
+++ menu/bckpbot.sh
@@ -27,7 +27,7 @@ checking_sc() {
         echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
         echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
         echo -e "             \033[0;33mContact Admin :${NC}"
-        echo -e "     \033[0;36mTelegram${NC}: https://t.me/diwayaa"
+        echo -e "     \033[0;36mTelegram${NC}: https://t.me/malayaacx_2601"
         echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
         exit
     fi
