fa3456932a94e14d1bca77062b5d8cb033a0b1bb
Malayaa Official <111335173+malayaacx01@users.noreply.github.com>
2024-12-09 22:40:45 +0800

    Update ohp.sh

--- sshws/ohp.sh
+++ sshws/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
-wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
+wget https://raw.githubusercontent.com/malayaacx01/Malayaacx-Autoscript-V2/refs/heads/main/ohpserver-linux32.zip
 unzip ohpserver-linux32.zip
 chmod +x ohpserver
 cp ohpserver /usr/local/bin/ohpserver
