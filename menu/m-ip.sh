c223ac9f86e727374282089f5fb5823ee18ec971
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-09 11:41:16 +0800

    Update m-ip.sh

--- menu/m-ip.sh
+++ menu/m-ip.sh
@@ -27,7 +27,7 @@ menu
 }
 checking_sc
 clear
-Repo1="https://raw.githubusercontent.com/malayaacx01/reg/main/"
+Repo1="https://reg.malayaacx.my.id/"
 export MYIP=$( curl -s https://ipinfo.io/ip/ )
 SELLER=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $2}')
 Exp100=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $3}')
