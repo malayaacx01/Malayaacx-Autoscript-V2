71bb0bd4942db166acfb8f322a2172339b94a8e0
malayaacx01 <111335173+malayaacx01@users.noreply.github.com>
2024-12-09 10:57:01 +0800

    Update ohp.sh

--- sshws/ohp.sh
+++ sshws/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
-wget https://scvpsv2.malayaacx.my.id/ohpserver-linux32.zip
+wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
 unzip ohpserver-linux32.zip
 chmod +x ohpserver
 cp ohpserver /usr/local/bin/ohpserver
